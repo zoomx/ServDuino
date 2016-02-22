be148f8982db51679d20385a4ca4b80874ac7c9b
zoomx <zoomx@tiscalinet.it>
2016-02-22 14:45:22 +0100

    Added DHCP and some updates. Now compile on IDE 1.6.7
       Modifications by Zoomx 2016
       Added DHCP and IP address print on serial
       Updated to the new Ethernet library
       Compile on IDE 1.6.7

--- ServDuino/RAMWebServer_Basic/RAMWebServer_Basic.ino
+++ ServDuino/RAMWebServer_Basic/RAMWebServer_Basic.ino
@@ -1,27 +1,64 @@
+/*
+   RAMWebServer_Basic
+    by Teslaling
+    http://www.instructables.com/id/ServDuino-Arduino-Webserver/?ALLSTEPS
+    2011
+
+   Modifications by Zoomx 2016
+   Added DHCP and IP address print on serial
+   Updated to the new Ethernet library
+   Compile on IDE 1.6.7
+*/
 #include <SPI.h>
 #include <Ethernet.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 192,168,1, 177 };
+//byte ip[] = { 192,168,1, 177 };
+IPAddress ip(192, 168, 1, 177);
+IPAddress myDns(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 // Initialize the Ethernet server library
-// with the IP address and port you want to use 
+// with the IP address and port you want to use
 // (port 80 is default for HTTP):
-Server server(80);
+EthernetServer server(80);
 
-void setup()
-{
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
+void setup() {
+  Serial.begin(9600);
+  // this check is only needed on the Leonardo:
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("RAMWebServer_Basic");
+
+
+  // start the Ethernet connection:
+  Serial.println("Trying to get an IP address using DHCP");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // initialize the Ethernet device not using DHCP:
+    Ethernet.begin(mac, ip, myDns, gateway, subnet);
+  }
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  ip = Ethernet.localIP();
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(ip[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+
   server.begin();
 }
 
 void loop()
 {
   // listen for incoming clients
-  Client client = server.available();
+  EthernetClient client = server.available();
   if (client) {
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
@@ -36,13 +73,14 @@ void loop()
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println();
-          client.println("your html code will go here. You cannot have any double quotes in it, however, because that will end the client print function "like this");
+          client.println("your html code will go here. You cannot have any double quotes in it, however, because that will end the client print function \"like this");
+          //You can, just add a \ before it! by Zoomx
           break;
         }
         if (c == '\n') {
           // you're starting a new line
           currentLineIsBlank = true;
-        } 
+        }
         else if (c != '\r') {
           // you've gotten a character on the current line
           currentLineIsBlank = false;
